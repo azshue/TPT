63ecbace79694205d7884e63fdc3137a200f0b0e
azshue <azure.Shu1997@gmail.com>
2022-10-14 20:55:46 -0400

    scripts

--- data/Untitled-1.sh
+++
@@ -1,3 +0,0 @@
-python ./tta_bongard.py /bongard --train_set bongard --bongard_split unseen_obj_unseen_act --ctx_init a_photo_of_a -a RN50 --pgen coop -b 64 --crop_scale 0.5 --severity 1.0 --aug_ops empty --tta_steps 1 --tta_lr 5e-3 --tta_optim AdamW --gpu 0 -p 10 --savedir temp
-            
-            
